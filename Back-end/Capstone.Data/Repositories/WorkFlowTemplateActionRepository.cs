ea0a85b97d463c33282d692c82de2f7e1ffc5965
Kietna <orionkaito@gmail.com>
2019-06-16 17:08:43 +0700

    16062019

---
+++ Back-end/Capstone.Data/Repositories/WorkFlowTemplateActionRepository.cs
@@ -0,0 +1,21 @@
+﻿using Capstone.Data.Infrastructrure;
+using Capstone.Model;
+using System.Linq;
+
+namespace Capstone.Data.Repositories
+{
+    public interface IWorkFlowTemplateActionRepository : IRepository<WorkFlowTemplateAction>
+    {
+        WorkFlowTemplateAction GetByName(string name);
+    }
+
+    public class WorkFlowTemplateActionRepository : RepositoryBase<WorkFlowTemplateAction>, IWorkFlowTemplateActionRepository
+    {
+        public WorkFlowTemplateActionRepository(IDbFactory dbFactory) : base(dbFactory) { }
+
+        public WorkFlowTemplateAction GetByName(string name)
+        {
+            return DbContext.WorkFlowTemplateActions.Where(w => w.Name.Equals(name)).FirstOrDefault();
+        }
+    }
+}
