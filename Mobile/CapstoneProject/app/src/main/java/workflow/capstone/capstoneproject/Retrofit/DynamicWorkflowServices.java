340409e86ee8d621c21cbee0a68a322e044afb5b
kevinz2019 <kevinz1997st@gmail.com>
2019-06-26 17:01:11 +0700

    android send request

--- Mobile/CapstoneProject/app/src/main/java/workflow/capstone/capstoneproject/Retrofit/DynamicWorkflowServices.java
+++ Mobile/CapstoneProject/app/src/main/java/workflow/capstone/capstoneproject/Retrofit/DynamicWorkflowServices.java
@@ -1,14 +1,17 @@
-package workflow.capstone.capstoneproject.Retrofit;
+package workflow.capstone.capstoneproject.retrofit;
 
 import java.util.Map;
 
+import okhttp3.MultipartBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
+import workflow.capstone.capstoneproject.api.Request;
 
 public interface DynamicWorkflowServices {
     @Headers({"Content-Type:application/json"})
@@ -16,5 +19,33 @@ public interface DynamicWorkflowServices {
     Call<ResponseBody> login(@Body Map<String, String> fields);
 
     @GET(ConfigApi.Api.GET_PROFILE)
-    Call<ResponseBody> getProfile(@Header("Authorization") String authorization);
+    Call<ResponseBody> getProfile();
+
+    @GET(ConfigApi.Api.GET_WORKFLOW)
+    Call<ResponseBody> getWorkflow();
+
+    @GET(ConfigApi.Api.GET_NUMBER_NOTIFICATION)
+    Call<ResponseBody> getNumberNotification();
+
+    @GET(ConfigApi.Api.GET_NOTIFICATION)
+    Call<ResponseBody> getNotification();
+
+    @Headers({"Content-Type:application/json"})
+    @POST(ConfigApi.Api.POST_REQUEST)
+    Call<ResponseBody> postRequest(@Body Request request);
+
+    @GET("https://demo6278327.mockable.io/GetForm")
+    Call<ResponseBody> getForm();
+
+    @Multipart
+    @POST(ConfigApi.Api.POST_REQUEST_FILE)
+    Call<ResponseBody> upFile(
+            @Part MultipartBody.Part file
+    );
+
+    @Multipart
+    @POST(ConfigApi.Api.POST_REQUEST_FILE)
+    Call<ResponseBody> uploadMultipleFile(
+            @Part MultipartBody.Part[] files
+    );
 }
--- Mobile/CapstoneProject/app/src/main/java/workflow/capstone/capstoneproject/activity/SplashActivity.java
+++ Mobile/CapstoneProject/app/src/main/java/workflow/capstone/capstoneproject/activity/SplashActivity.java
@@ -1,10 +1,9 @@
 package workflow.capstone.capstoneproject.activity;
 
-import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import workflow.capstone.capstoneproject.R;
 import workflow.capstone.capstoneproject.utils.ConstantDataManager;
