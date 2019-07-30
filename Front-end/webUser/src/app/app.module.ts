c3d359f6a64374787de2c59c9ae29906911526dc
linhvdse3101 <linhvdse3101@gmail.com>
2019-07-30 22:18:56 +0700

    Fix dynamic form

--- Front-end/webUser/src/app/app.module.ts
+++ Front-end/webUser/src/app/app.module.ts
@@ -15,7 +15,7 @@ import {
 } from '@agm/core';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { CommonModule } from '@angular/common';
-import { MatTableModule, MatTabsModule, MatIconModule, MatPaginatorModule, MatSortModule, MatDialogModule, MatSelectModule, MatFormFieldModule, MatChipsModule, MatInputModule } from '@angular/material';
+import { MatTableModule, MatTabsModule, MatCheckboxModule, MatIconModule, MatPaginatorModule, MatSortModule, MatDialogModule, MatSelectModule, MatFormFieldModule, MatChipsModule, MatInputModule } from '@angular/material';
 import { CdkTable, CdkTableModule } from '@angular/cdk/table';
 import { AddAccountComponent } from './add-account/add-account.component';
 import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
@@ -76,6 +76,7 @@ const config = {
     ComponentsModule,
     HttpClientModule,
     MatDialogModule,
+    MatCheckboxModule,
     RouterModule,
     AppRoutingModule,
     FormsModule,
