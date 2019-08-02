6dd53c28ffccbaa08cb26b7a046c6b132394fb2b
linhvdse3101 <linhvdse3101@gmail.com>
2019-08-02 23:41:28 +0700

    fixlayout 2/8

--- Front-end/webUser/src/app/useClass/combo-element.ts
+++ Front-end/webUser/src/app/useClass/combo-element.ts
@@ -1,16 +1,16 @@
 export class ComboElement {
     constructor(){
-        this.textOnly={name:""},
-        this.shortText= {name: "", value: ""},
-        this.longText = {name: "", value: ""},
-        this.comboBox = {name: "", value: "", valueOfProper: []},
-        this.inputCheckbox = {name: "", value: ""}
+        this.textOnly={name:"", class:""},
+        this.shortText= {name: "", value: "", class:""},
+        this.longText = {name: "", value: "", class:""},
+        this.comboBox = {name: "", value: "", valueOfProper: [], class:""},
+        this.inputCheckbox = {name: "", value: "", class:""}
     };
-    textOnly: {name: string};
-    shortText: {name: string, value: string};
-    longText: {name: string, value: string};
-    comboBox: {name: string, value: string, valueOfProper: any};
-    inputCheckbox: {name: string, value: string};
+    textOnly: {name: string, class: string};
+    shortText: {name: string, value: string,  class: string};
+    longText: {name: string, value: string,  class: string};
+    comboBox: {name: string, value: string, valueOfProper: any, class: string;}
+    inputCheckbox: {name: string, value: string,  class: string};
     
 }
 
